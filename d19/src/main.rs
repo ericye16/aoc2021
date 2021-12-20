bbb632b976e597da59c27fdaad532395d8ba0ad8
Eric Ye <me@ericye16.com>
2021-12-20 00:05:53 -0500

    Day 19 done (so difficult, runs very slowly)

--- d19/src/main.rs
+++ d19/src/main.rs
@@ -1,44 +1,73 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 
 use lazy_static::lazy_static;
+use ndarray::*;
+// use ndarray_linalg::*;
 use text_io::try_scan;
 
-fn p1(input: &str) -> i32 {
-    todo!()
+type Coord = Array1<i32>;
+type Rotation = Array2<i32>;
+
+lazy_static! {
+    static ref ROTATIONS: Vec<Rotation> = generate_rotations();
+    // static ref ROTATION_INV: Vec<Rotation> = ROTATIONS
+    //     .iter()
+    //     .map(|r| r.map(|x| *x as f32).inv().unwrap())
+    //     .map(|ri| ri.map(|x| *x as i32))
+    //     .collect();
 }
 
-fn p2(input: &str) -> i32 {
-    todo!()
-}
+fn generate_rotations() -> Vec<Array2<i32>> {
+    // Four rotations about the x axis (only roll)
+    let rotations: Vec<Array2<i32>> = (0..4)
+        .map(|roll| {
+            array![
+                [1, 0, 0],
+                [0, intcos(roll), -intsin(roll)],
+                [0, intsin(roll), intcos(roll)],
+            ]
+        })
+        .collect();
 
-type Coord = ndarray::Array1<i32>;
+    let mut v = vec![];
 
-/*
-#[derive(Debug, Clone, PartialEq, Eq, Copy, Hash)]
-struct Coords(i32, i32, i32);
-
-impl Display for Coords {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "({},{},{})", self.0, self.1, self.2)
+    // +/- x
+    for yaw in [0, 2] {
+        let m = array![
+            [intcos(yaw), -intsin(yaw), 0],
+            [intsin(yaw), intcos(yaw), 0],
+            [0, 0, 1]
+        ];
+        for rotation in &rotations {
+            v.push(m.dot(rotation));
+        }
     }
-}
 
-impl Sub for Coords {
-    type Output = Coords;
-
-    fn sub(self, rhs: Self) -> Self::Output {
-        Coords(self.0 - rhs.0, self.1 - rhs.1, self.2 - rhs.2)
+    // +/- y
+    for yaw in [1, 3] {
+        let m = array![
+            [intcos(yaw), -intsin(yaw), 0],
+            [intsin(yaw), intcos(yaw), 0],
+            [0, 0, 1]
+        ];
+        for rotation in &rotations {
+            v.push(m.dot(rotation));
+        }
     }
-}
 
-impl Add for Coords {
-    type Output = Coords;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        Coords(self.0 + rhs.0, self.1 + rhs.1, self.2 + rhs.2)
+    // +/- z
+    for pitch in [1, 3] {
+        let m = array![
+            [intcos(pitch), 0, intsin(pitch)],
+            [0, 1, 0],
+            [-intsin(pitch), 0, intcos(pitch)]
+        ];
+        for rotation in &rotations {
+            v.push(m.dot(rotation));
+        }
     }
+    v
 }
-*/
 
 fn maybe_parse_scanner(line: &str) -> Result<usize, Box<dyn std::error::Error>> {
     let id: usize;
@@ -46,7 +75,7 @@ fn maybe_parse_scanner(line: &str) -> Result<usize, Box<dyn std::error::Error>> 
     Ok(id)
 }
 
-fn parse_coords(line: &str) -> ndarray::Array1<i32> {
+fn parse_coords(line: &str) -> Array1<i32> {
     let coords: Vec<&str> = line.split(',').collect();
     assert!(coords.len() >= 2);
     let x = coords[0].parse().unwrap();
@@ -56,7 +85,7 @@ fn parse_coords(line: &str) -> ndarray::Array1<i32> {
     } else {
         0
     };
-    ndarray::array![x, y, z]
+    array![x, y, z]
 }
 
 fn parse_input(input: &str) -> Vec<HashSet<Coord>> {
@@ -79,14 +108,17 @@ fn count_matching(
     reference: &HashSet<Coord>,
     to_align: &HashSet<Coord>,
     correction: &Coord,
+    rotation: &Rotation,
 ) -> i32 {
     let mut c = 0;
     for target in to_align {
-        let corrected = target + correction;
+        let rotated = rotation.dot(target);
+        let corrected = rotated + correction;
         if reference.contains(&corrected) {
             c += 1;
         }
     }
+    assert!(c >= 1);
     c
 }
 
@@ -110,89 +142,225 @@ fn intcos(i: i32) -> i32 {
     }
 }
 
-lazy_static! {
-    static ref ROTATIONS: Vec<ndarray::Array2<i32>> = generate_rotations();
-}
-
-fn generate_rotations() -> Vec<ndarray::Array2<i32>> {
-    // Four rotations about the x axis (only roll)
-    let rotations: Vec<ndarray::Array2<i32>> = (0..4)
-        .map(|roll| {
-            ndarray::array![
-                [1, 0, 0],
-                [0, intcos(roll), -intsin(roll)],
-                [0, intsin(roll), intcos(roll)],
-            ]
-        })
-        .collect();
-
-    let mut v = vec![];
-
-    // +/- x
-    for yaw in [0, 2] {
-        let m = ndarray::array![
-            [intcos(yaw), -intsin(yaw), 0],
-            [intsin(yaw), intcos(yaw), 0],
-            [0, 0, 1]
-        ];
-        for rotation in &rotations {
-            v.push(m.dot(rotation));
-        }
-    }
-
-    // +/- y
-    for yaw in [1, 3] {
-        let m = ndarray::array![
-            [intcos(yaw), -intsin(yaw), 0],
-            [intsin(yaw), intcos(yaw), 0],
-            [0, 0, 1]
-        ];
-        for rotation in &rotations {
-            v.push(m.dot(rotation));
-        }
-    }
-
-    // +/- z
-    for pitch in [1, 3] {
-        let m = ndarray::array![
-            [intcos(pitch), 0, intsin(pitch)],
-            [0, 1, 0],
-            [-intsin(pitch), 0, intcos(pitch)]
-        ];
-        for rotation in &rotations {
-            v.push(m.dot(rotation));
-        }
-    }
-    v
-}
-
 fn try_align(
     reference: &HashSet<Coord>,
     to_align: &HashSet<Coord>,
     threshold: i32,
-) -> Option<Coord> {
+) -> Option<(Coord, Rotation)> {
     for ref_beacon in reference {
         for target_beacon in to_align {
-            let correction = ref_beacon - target_beacon;
-            let num_matching = count_matching(reference, to_align, &correction);
-            println!("Correction: {}, matching: {}", correction, num_matching);
-            if num_matching >= threshold {
-                return Some(correction);
+            for rotation in ROTATIONS.iter() {
+                // we want reference = rotation.dot(target) + correction
+                // so correction = reference - rotation.dot(target)
+                let correction = ref_beacon - rotation.dot(target_beacon);
+                let num_matching = count_matching(reference, to_align, &correction, rotation);
+                // println!("Correction: {}, matching: {}", correction, num_matching);
+                if num_matching >= threshold {
+                    return Some((correction, rotation.clone()));
+                }
             }
         }
     }
     None
 }
 
+fn coalesce_all(input: &Vec<HashSet<Coord>>) -> (HashSet<Coord>, Vec<Coord>) {
+    let mut known = input[0].clone();
+    let mut unmatched: VecDeque<HashSet<Coord>> = input[1..].iter().cloned().collect();
+    let mut scanners = vec![array![0, 0, 0]];
+    while let Some(to_match) = unmatched.pop_front() {
+        println!(
+            "There are {} scanners remaining to be solved. {} beacons found so far.",
+            unmatched.len(),
+            known.len()
+        );
+        if let Some((correction, rotation)) = try_align(&known, &to_match, 12) {
+            // add vectors with corrections to known
+            for scanned in to_match {
+                let corrected = rotation.dot(&scanned) + &correction;
+                known.insert(corrected);
+            }
+            scanners.push(correction);
+        } else {
+            unmatched.push_back(to_match);
+        }
+    }
+    (known, scanners)
+}
+
+fn p1(input: &str) -> i32 {
+    let input = parse_input(input);
+    coalesce_all(&input).0.len() as i32
+}
+
+fn manhattan_dist(a: &Coord, b: &Coord) -> i32 {
+    (a[0] - b[0]).abs() + (a[1] - b[1]).abs() + (a[2] - b[2]).abs()
+}
+
+fn p2(input: &str) -> i32 {
+    let input = parse_input(input);
+    let scanners = coalesce_all(&input).1;
+
+    let mut m = 0;
+    for i in 0..scanners.len() {
+        for j in 0..scanners.len() {
+            if i == j {
+                continue;
+            }
+            let md = manhattan_dist(&scanners[i], &scanners[j]);
+            if md > m {
+                m = md;
+            }
+        }
+    }
+    m
+}
 fn main() {
-    // let input = common::read_file("d19.txt");
-    // println!("P1: {}", p1(input.trim()));
-    // println!("P2: {}", p2(input.trim()));
+    let input = common::read_file("d19.txt");
+    println!("P1: {}", p1(input.trim()));
+    println!("P2: {}", p2(input.trim()));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    const INPUT: &str = "--- scanner 0 ---
+    404,-588,-901
+    528,-643,409
+    -838,591,734
+    390,-675,-793
+    -537,-823,-458
+    -485,-357,347
+    -345,-311,381
+    -661,-816,-575
+    -876,649,763
+    -618,-824,-621
+    553,345,-567
+    474,580,667
+    -447,-329,318
+    -584,868,-557
+    544,-627,-890
+    564,392,-477
+    455,729,728
+    -892,524,684
+    -689,845,-530
+    423,-701,434
+    7,-33,-71
+    630,319,-379
+    443,580,662
+    -789,900,-551
+    459,-707,401
+    
+    --- scanner 1 ---
+    686,422,578
+    605,423,415
+    515,917,-361
+    -336,658,858
+    95,138,22
+    -476,619,847
+    -340,-569,-846
+    567,-361,727
+    -460,603,-452
+    669,-402,600
+    729,430,532
+    -500,-761,534
+    -322,571,750
+    -466,-666,-811
+    -429,-592,574
+    -355,545,-477
+    703,-491,-529
+    -328,-685,520
+    413,935,-424
+    -391,539,-444
+    586,-435,557
+    -364,-763,-893
+    807,-499,-711
+    755,-354,-619
+    553,889,-390
+    
+    --- scanner 2 ---
+    649,640,665
+    682,-795,504
+    -784,533,-524
+    -644,584,-595
+    -588,-843,648
+    -30,6,44
+    -674,560,763
+    500,723,-460
+    609,671,-379
+    -555,-800,653
+    -675,-892,-343
+    697,-426,-610
+    578,704,681
+    493,664,-388
+    -671,-858,530
+    -667,343,800
+    571,-461,-707
+    -138,-166,112
+    -889,563,-600
+    646,-828,498
+    640,759,510
+    -630,509,768
+    -681,-892,-333
+    673,-379,-804
+    -742,-814,-386
+    577,-820,562
+    
+    --- scanner 3 ---
+    -589,542,597
+    605,-692,669
+    -500,565,-823
+    -660,373,557
+    -458,-679,-417
+    -488,449,543
+    -626,468,-788
+    338,-750,-386
+    528,-832,-391
+    562,-778,733
+    -938,-730,414
+    543,643,-506
+    -524,371,-870
+    407,773,750
+    -104,29,83
+    378,-903,-323
+    -778,-728,485
+    426,699,580
+    -438,-605,-362
+    -469,-447,-387
+    509,732,623
+    647,635,-688
+    -868,-804,481
+    614,-800,639
+    595,780,-596
+    
+    --- scanner 4 ---
+    727,592,562
+    -293,-554,779
+    441,611,-461
+    -714,465,-776
+    -743,427,-804
+    -660,-479,-426
+    832,-632,460
+    927,-485,-438
+    408,393,-506
+    466,436,-512
+    110,16,151
+    -258,-428,682
+    -393,719,612
+    -211,-452,876
+    808,-476,-593
+    -575,615,604
+    -485,667,467
+    -680,325,-822
+    -627,-443,-432
+    872,-547,-609
+    833,512,582
+    807,604,487
+    839,-516,451
+    891,-625,532
+    -652,-548,-490
+    30,-46,-14";
 
     #[test]
     fn test_2d() {
@@ -208,7 +376,7 @@ mod tests {
         -2,1",
         );
         let correction = try_align(&input[0], &input[1], 3);
-        assert_eq!(correction.unwrap(), ndarray::array![5, 2, 0]);
+        assert_eq!(correction.unwrap(), (array![5, 2, 0], ArrayBase::eye(3)));
     }
 
     #[test]
@@ -222,7 +390,7 @@ mod tests {
             .lines()
             .map(str::trim)
             .map(parse_coords)
-            .collect::<Vec<ndarray::Array1<i32>>>();
+            .collect::<Vec<Array1<i32>>>();
 
         let v2 = "1,-1,1
         2,-2,2
@@ -233,7 +401,7 @@ mod tests {
             .lines()
             .map(str::trim)
             .map(parse_coords)
-            .collect::<Vec<ndarray::Array1<i32>>>();
+            .collect::<Vec<Array1<i32>>>();
 
         let v3 = "-1,-1,-1
         -2,-2,-2
@@ -244,7 +412,7 @@ mod tests {
             .lines()
             .map(str::trim)
             .map(parse_coords)
-            .collect::<Vec<ndarray::Array1<i32>>>();
+            .collect::<Vec<Array1<i32>>>();
 
         let v4 = "1,1,-1
         2,2,-2
@@ -255,7 +423,7 @@ mod tests {
             .lines()
             .map(str::trim)
             .map(parse_coords)
-            .collect::<Vec<ndarray::Array1<i32>>>();
+            .collect::<Vec<Array1<i32>>>();
 
         let v5 = "1,1,1
         2,2,2
@@ -266,14 +434,14 @@ mod tests {
             .lines()
             .map(str::trim)
             .map(parse_coords)
-            .collect::<Vec<ndarray::Array1<i32>>>();
+            .collect::<Vec<Array1<i32>>>();
 
         for i in 0..v1.len() {
             let r = &v1[i];
             let rotated_r = ROTATIONS
                 .iter()
                 .map(|rm| rm.dot(r))
-                .collect::<HashSet<ndarray::Array1<i32>>>();
+                .collect::<HashSet<Array1<i32>>>();
             println!("r: {}, rotated_r: {:?}", r, rotated_r);
             assert!(rotated_r.contains(&v1[i]));
             assert!(rotated_r.contains(&v2[i]));
@@ -284,105 +452,19 @@ mod tests {
     }
 
     #[test]
+    fn test_scanner_locate() {
+        let input = parse_input(INPUT);
+        let s1 = try_align(&input[0], &input[1], 12).unwrap();
+        assert_eq!(s1.0, array![68, -1246, -43]);
+    }
+
+    #[test]
     fn test_p1() {
-        assert_eq!(
-            p1("-892,524,684
-            -876,649,763
-            -838,591,734
-            -789,900,-551
-            -739,-1745,668
-            -706,-3180,-659
-            -697,-3072,-689
-            -689,845,-530
-            -687,-1600,576
-            -661,-816,-575
-            -654,-3158,-753
-            -635,-1737,486
-            -631,-672,1502
-            -624,-1620,1868
-            -620,-3212,371
-            -618,-824,-621
-            -612,-1695,1788
-            -601,-1648,-643
-            -584,868,-557
-            -537,-823,-458
-            -532,-1715,1894
-            -518,-1681,-600
-            -499,-1607,-770
-            -485,-357,347
-            -470,-3283,303
-            -456,-621,1527
-            -447,-329,318
-            -430,-3130,366
-            -413,-627,1469
-            -345,-311,381
-            -36,-1284,1171
-            -27,-1108,-65
-            7,-33,-71
-            12,-2351,-103
-            26,-1119,1091
-            346,-2985,342
-            366,-3059,397
-            377,-2827,367
-            390,-675,-793
-            396,-1931,-563
-            404,-588,-901
-            408,-1815,803
-            423,-701,434
-            432,-2009,850
-            443,580,662
-            455,729,728
-            456,-540,1869
-            459,-707,401
-            465,-695,1988
-            474,580,667
-            496,-1584,1900
-            497,-1838,-617
-            527,-524,1933
-            528,-643,409
-            534,-1912,768
-            544,-627,-890
-            553,345,-567
-            564,392,-477
-            568,-2007,-577
-            605,-1665,1952
-            612,-1593,1893
-            630,319,-379
-            686,-3108,-505
-            776,-3184,-501
-            846,-3110,-434
-            1135,-1161,1235
-            1243,-1093,1063
-            1660,-552,429
-            1693,-557,386
-            1735,-437,1738
-            1749,-1800,1813
-            1772,-405,1572
-            1776,-675,371
-            1779,-442,1789
-            1780,-1548,337
-            1786,-1538,337
-            1847,-1591,415
-            1889,-1729,1762
-            1994,-1805,1792"),
-            4140
-        );
+        assert_eq!(p1(INPUT), 79);
     }
 
     #[test]
     fn test_p2() {
-        assert_eq!(
-            p2("[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
-        [[[5,[2,8]],4],[5,[[9,9],0]]]
-        [6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
-        [[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
-        [[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
-        [[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
-        [[[[5,4],[7,7]],8],[[8,3],8]]
-        [[9,3],[[9,9],[6,[4,9]]]]
-        [[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
-        [[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"),
-            3993
-        );
+        assert_eq!(p2(INPUT), 3621);
     }
 }
