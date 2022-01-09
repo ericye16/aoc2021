c0e287d89cefec7ba7a391eddbe9029b0f35d685
Eric Ye <me@ericye16.com>
2022-01-09 15:57:12 -0800

    Used enums instead of string comparisons

--- d12/src/main.rs
+++ d12/src/main.rs
@@ -1,22 +1,20 @@
 use std::collections::{HashMap, HashSet};
 
-type Cave<'a> = &'a str;
-
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum Cave<'a> {
+    Start,
+    End,
+    Small(&'a str),
+    Big(&'a str),
+}
 type CaveSystem<'a> = HashMap<Cave<'a>, Vec<Cave<'a>>>;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum CaveType {
-    Small,
-    Big,
-}
-
-fn get_cave_type(cave: &Cave) -> CaveType {
-    if cave.chars().all(char::is_uppercase) {
-        CaveType::Big
-    } else if cave.chars().all(char::is_lowercase) {
-        CaveType::Small
-    } else {
-        panic!("Mixed case cave");
+fn to_cave<'a>(cave_name: &'a str) -> Cave<'a> {
+    match cave_name {
+        "start" => Cave::Start,
+        "end" => Cave::End,
+        cave_name if cave_name.chars().all(char::is_lowercase) => Cave::Small(cave_name),
+        _ => Cave::Big(cave_name),
     }
 }
 
@@ -28,90 +26,91 @@ fn parse_cave_system(input: &str) -> CaveSystem {
         let first_cave = cave_connections[0];
         let second_cave = cave_connections[1];
         cave_system
-            .entry(first_cave)
+            .entry(to_cave(first_cave))
             .or_insert(vec![])
-            .push(second_cave);
+            .push(to_cave(second_cave));
         // Bidirectional cave systems
         cave_system
-            .entry(second_cave)
+            .entry(to_cave(second_cave))
             .or_insert(vec![])
-            .push(first_cave);
+            .push(to_cave(first_cave));
     }
     cave_system
 }
 
-fn find_paths<'a>(cave_system: &'a CaveSystem, last_cave: &str, used_caves: HashSet<&str>) -> i32 {
-    if last_cave == "end" {
-        return 1;
-    }
-    if !cave_system.contains_key(last_cave) {
-        panic!("Cave system doesn't contain us")
-    }
-    let mut num_caves = 0;
-    for possible_next_cave in cave_system.get(last_cave).unwrap() {
-        if used_caves.contains(*possible_next_cave) {
-            continue;
+fn find_paths<'a>(
+    cave_system: &'a CaveSystem,
+    last_cave: &Cave,
+    used_caves: HashSet<&Cave>,
+) -> i32 {
+    match last_cave {
+        Cave::End => 1,
+        _ => {
+            debug_assert!(cave_system.contains_key(&last_cave));
+            let mut num_caves = 0;
+            for possible_next_cave in cave_system.get(&last_cave).unwrap() {
+                if used_caves.contains(possible_next_cave) {
+                    continue;
+                }
+                let mut this_used_caves = used_caves.clone();
+                if let Cave::Small(_) = possible_next_cave {
+                    this_used_caves.insert(possible_next_cave);
+                }
+                num_caves += find_paths(cave_system, possible_next_cave, this_used_caves);
+            }
+            num_caves
         }
-        let mut this_used_caves = used_caves.clone();
-        if get_cave_type(possible_next_cave) == CaveType::Small {
-            this_used_caves.insert(possible_next_cave);
-        }
-        num_caves += find_paths(cave_system, possible_next_cave, this_used_caves);
     }
-    num_caves
 }
 
 fn p1(input: &str) -> i32 {
     let cave_system = parse_cave_system(input);
     let mut used_caves = HashSet::new();
-    used_caves.insert("start");
-    let paths = find_paths(&cave_system, "start", used_caves);
+    used_caves.insert(&Cave::Start);
+    let paths = find_paths(&cave_system, &Cave::Start, used_caves);
     paths
 }
 
 fn find_paths_p2<'a>(
     cave_system: &'a CaveSystem,
-    last_cave: &str,
-    used_caves: HashSet<&str>,
+    last_cave: &Cave,
+    used_caves: HashSet<&Cave>,
     used_2_caves: bool,
 ) -> i32 {
-    if last_cave == "end" {
-        // println!("Found complete path: {:?}", path);
-        return 1;
-    }
-    if !cave_system.contains_key(last_cave) {
-        panic!("Cave system doesn't contain us")
-    }
-    let mut s = 0;
-    for possible_next_cave in cave_system.get(last_cave).unwrap() {
-        if *possible_next_cave == "start" {
-            continue;
+    match last_cave {
+        Cave::End => 1,
+        _ => {
+            debug_assert!(cave_system.contains_key(&last_cave));
+            let mut s = 0;
+            for possible_next_cave in cave_system.get(last_cave).unwrap() {
+                if *possible_next_cave == Cave::Start {
+                    continue;
+                }
+                if used_2_caves && used_caves.contains(possible_next_cave) {
+                    continue;
+                }
+                let mut this_used_caves = used_caves.clone();
+                let mut this_used_2_caves = used_2_caves;
+                if let Cave::Small(_) = possible_next_cave {
+                    this_used_2_caves |= !this_used_caves.insert(possible_next_cave);
+                }
+                s += find_paths_p2(
+                    cave_system,
+                    possible_next_cave,
+                    this_used_caves,
+                    this_used_2_caves,
+                );
+            }
+            s
         }
-        if used_2_caves && used_caves.contains(possible_next_cave) {
-            continue;
-        }
-        let mut this_used_caves = used_caves.clone();
-        let mut this_used_2_caves = used_2_caves;
-        if get_cave_type(possible_next_cave) == CaveType::Small {
-            this_used_2_caves |= !this_used_caves.insert(possible_next_cave);
-        }
-        s += find_paths_p2(
-            cave_system,
-            possible_next_cave,
-            this_used_caves,
-            this_used_2_caves,
-        );
     }
-    s
 }
 
 fn p2(input: &str) -> i32 {
     let cave_system = parse_cave_system(input);
-    // println!("Cave system: {:?}", cave_system);
     let mut used_caves = HashSet::new();
-    used_caves.insert("start");
-    let paths = find_paths_p2(&cave_system, "start", used_caves, false);
-    // println!("Paths: {:?}", paths);
+    used_caves.insert(&Cave::Start);
+    let paths = find_paths_p2(&cave_system, &Cave::Start, used_caves, false);
     paths
 }
 
